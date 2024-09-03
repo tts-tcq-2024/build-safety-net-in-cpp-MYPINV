1fd396d87d18053ed9acfdfbacbf1cfa7fd3e8a6
Ankit Kumar <83908838+MYPINV@users.noreply.github.com>
2024-09-03 10:21:33 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -16,8 +16,14 @@ TEST(SoundexTest, AllVowels) {
     EXPECT_EQ(generateSoundex("Euio"), "E000");
 }
 
-TEST(SoundexTest, UnspecifiedCharacters) {
+TEST(SoundexTest, UndefinedCharacters) {
     EXPECT_EQ(generateSoundex("yhwh"), "Y000");
+}
+
+TEST(SoundexTest, CaseInsensitive) {
+    EXPECT_EQ(generateSoundex("SINGH"), "S520");
+    EXPECT_EQ(generateSoundex("PriNcE"), "P652");
+    EXPECT_EQ(generateSoundex("cAtS"), "C320");
 }
 
 
