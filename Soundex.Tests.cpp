73477dd9d6984260deb3041c38f23afe9dd59825
Ankit Kumar <83908838+MYPINV@users.noreply.github.com>
2024-09-03 10:13:07 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -8,5 +8,16 @@ TEST(SoundexTest, HandlesEmptyString) {
 
 TEST(SoundexTest, HandlesSingleCharacter) {
     EXPECT_EQ(generateSoundex("A"), "A000");
+    EXPECT_EQ(generateSoundex("F"), "F000");
 }
+
+TEST(SoundexTest, AllVowels) {
+    EXPECT_EQ(generateSoundex("Aeio"), "A000");
+    EXPECT_EQ(generateSoundex("Euio"), "E000");
+}
+
+TEST(SoundexTest, UnspecifiedCharacters) {
+    EXPECT_EQ(generateSoundex("yhwh"), "Y000");
+}
+
 
