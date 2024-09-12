903497e3f4b39594c6435a6811dd02c4d2f1b96c
Ankit Kumar <83908838+MYPINV@users.noreply.github.com>
2024-09-12 10:11:14 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,29 +1,41 @@
 #include <gtest/gtest.h>
 #include "Soundex.h"
 
+TEST(SoundexTest, SimpleName) {
+    EXPECT_EQ(generateSoundex("Smith"), "S530");
+}
 
-TEST(SoundexTest, HandlesEmptyString) {
+TEST(SoundexTest, VowelsIgnored) {
+    EXPECT_EQ(generateSoundex("Smyth"), "S530");
+}
+
+TEST(SoundexTest, RepeatingConsonants) {
+    EXPECT_EQ(generateSoundex("Ashcraft"), "A261");
+}
+
+TEST(SoundexTest, ShortName) {
+    EXPECT_EQ(generateSoundex("Li"), "L000");
+}
+
+TEST(SoundexTest, SingleLetterName) {
+    EXPECT_EQ(generateSoundex("A"), "A000");
+}
+
+TEST(SoundexTest, EmptyString) {
     EXPECT_EQ(generateSoundex(""), "");
 }
 
-TEST(SoundexTest, HandlesSingleCharacter) {
-    EXPECT_EQ(generateSoundex("A"), "A000");
-    EXPECT_EQ(generateSoundex("F"), "F000");
-}
-
-TEST(SoundexTest, AllVowels) {
-    EXPECT_EQ(generateSoundex("Aeio"), "A000");
-    EXPECT_EQ(generateSoundex("Euio"), "E000");
-}
-
-TEST(SoundexTest, UndefinedCharacters) {
-    EXPECT_EQ(generateSoundex("yhwh"), "Y000");
-}
-
 TEST(SoundexTest, CaseInsensitive) {
-    EXPECT_EQ(generateSoundex("SINGH"), "S520");
-    EXPECT_EQ(generateSoundex("PriNcE"), "P652");
-    EXPECT_EQ(generateSoundex("cAtS"), "C320");
+    EXPECT_EQ(generateSoundex("Washington"), generateSoundex("washington"));
 }
+
+TEST(SoundexTest, NonMatchingLetters) {
+    EXPECT_EQ(generateSoundex("Aeio"), "A000");
+}
+
+TEST(SoundexTest, NameWithSpaces) {
+    EXPECT_EQ(generateSoundex("  "), "");
+}
+
 
 
